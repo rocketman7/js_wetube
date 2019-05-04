5f5fc5dcc7283f3ea244964528fadef6159d1a50
rocketman <ryomajk@gmail.com>
2019-05-04 18:04:44 +0900

    before db

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
 import { videos } from "../db";
+import routes from "../routes";
 export const home = (req, res) => {
   res.render("home", { pageTitle: "Home", videos });
 };
@@ -9,8 +10,17 @@ export const search = (req, res) => {
   res.render("search", { pageTitle: "Search", searchingFor, videos });
 };
 
-export const upload = (req, res) =>
+export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
+
+export const postUpload = (req, res) => {
+  const {
+    body: { file, title, description }
+  } = req;
+  // To Do: upload and save videos
+  res.redirect(routes.videoDetail(154212));
+};
+
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
 export const editVideo = (req, res) =>
